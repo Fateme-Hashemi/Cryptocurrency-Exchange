8201b199d4cedc64769ef67f87dd14850e5c5eaa
Elahe Hashemi <elahe.hs481@gmail.com>
2023-07-24 20:06:42 +0330

    searchHandler and search filter added

--- src/components/Landing.js
+++ src/components/Landing.js
@@ -5,64 +5,48 @@ import Loading from "./Loading";
 import Coin from './Coin';
 
 
-
 const Landing = () => {
-
     const [coins, setCoins] = useState([]);
+    const [search, setSearch] = useState("");
 
+    useEffect(() => {
+        const fetchapi = async () => {
+            const data = await getapi();
+            setCoins(data);
+        }
+        fetchapi();
+    }, []);
 
-
- useEffect(()=> {
-    const fetchapi =async ()=> {
-        const data = await getapi();
-        setCoins(data);
+    const searchHandler = (event) => {
+        setSearch(event.target.value);
     }
 
-    fetchapi();
- }, [])
-
-
+    const searchcoins = coins.filter( coin => coin.name.toLowerCase().includes(search.toLowerCase()))
 
     return (
-   <>
-
-        
-        <input type='text' placeholder='Search ...' />
-
-      
+        <>
+            <input type='text' placeholder='Search ...' value={search} onChange={searchHandler} />
 
             {
                 coins.length ?
-                <div>
-                {
-                    coins.map(coin => <Coin 
-                    key={coin.id}
-                    name={coin.name}
-                    symbol={coin.symbol}
-                    price={coin.current_price}
-                    priceChange={coin.price_change_24h}
-                    marketcap={coin.market_cap}
-                    image={coin.image}
-                    
-                    />)
-                }
-    
-            </div> :
-
-                <Loading />
-
-
+                    <div>
+                        {
+                            searchcoins.map(coin => (
+                                <Coin
+                                    key={coin.id}
+                                    name={coin.name}
+                                    symbol={coin.symbol}
+                                    price={coin.current_price}
+                                    priceChange={coin.price_change_24h}
+                                    marketcap={coin.market_cap}
+                                    image={coin.image}
+                                />
+                            ))
+                        }
+                    </div> :
+                    <Loading />
             }
-
-
-
-       
-
-
-
-
-
-   </>
+        </>
     );
 };
 
